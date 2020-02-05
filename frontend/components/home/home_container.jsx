14c7109aa0814e25395bf0f3d77556bc9a4a10d4
Kevin <knguyen3415@gmail.com>
2020-02-05 10:28:25 -0800

    Fix session and shoe errors

--- frontend/components/home/home_container.jsx
+++ frontend/components/home/home_container.jsx
@@ -2,9 +2,13 @@ import { connect } from 'react-redux';
 import Home from './home';
 import { logout } from '../../actions/session_actions';
 
-const mSTP = (state) => ({
-    currentUser: state.session.id
-});
+const mSTP = (state) => {
+    let isLoggedIn = (state.session.currentUserId ? true : false);
+    return ({
+        loggedIn: isLoggedIn,
+        currentUser: (isLoggedIn ? state.entitites.currentUser : null)
+    })
+};
 
 const mDTP = dispatch => ({
     logout: () => dispatch(logout())

95eacf6091f695928f2d8397227c1fa780600be1
Kevin <knguyen3415@gmail.com>
2020-02-13 09:26:28 -0800

    Work on user profile frontend

--- frontend/reducers/entities/users_reducer.js
+++ frontend/reducers/entities/users_reducer.js
@@ -1,10 +1,18 @@
 import { RECEIVE_CURRENT_USER } from '../../actions/session_actions';
+import { RECEIVE_USER, DELETE_USER } from '../../actions/user_actions';
 
 const usersReducer = (oldState = {}, action) => {
 
     switch (action.type) {
-        case RECEIVE_CURRENT_USER:
-            return Object.assign({}, oldState, { [action.currentUser.id]: action.currentUser });
+        case RECEIVE_CURRENT_USER || RECEIVE_USER:
+            if (action.user){
+                Object.assign({}, oldState, action.user);
+            } else {
+                return state;
+            }
+
+        case DELETE_USER:
+            return {};
 
         default:
             return oldState;
--- frontend/reducers/errors/user_errors_reducer.js
+++ frontend/reducers/errors/user_errors_reducer.js
@@ -1,1 +1,15 @@
-import {}
+import { RECEIVE_USER, RECEIVE_USER_ERRORS } from '../../actions/user_actions';
+
+export default (state = [], action) => {
+    Object.freeze(state);
+    switch (action.type) {
+        case RECEIVE_USER_ERRORS:
+            return action.errors;
+
+        case RECEIVE_USER:
+            return [];
+    
+        default:
+            return state;
+    }
+};

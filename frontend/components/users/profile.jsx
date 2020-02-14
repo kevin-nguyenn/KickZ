340331fc27fde70a0d0a5004fd25760647eb15e9
Kevin <knguyen3415@gmail.com>
2020-02-14 13:45:30 -0800

    Add to profile navbar

---
+++ frontend/components/users/profile.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Route, Router } from 'react-router-dom';
+import { ProtectedRoute } from '../../util/route_util';
+import ProfileNavContainer from './profile_nav_container';
+import ProfileShowContainer from './profile_show_container';
+
+class Profile extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        <div className="profile">
+            <ProfileNavContainer/>
+            <div className="profile-contents">
+                <Route exact path="/profile" component={ProfileShowContainer} />
+                <ProtectedRoute/>
+            </div>
+        </div>
+    }
+}

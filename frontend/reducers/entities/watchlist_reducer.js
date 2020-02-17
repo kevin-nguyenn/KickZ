a752c64d73c26132ea884299a21d2baa381ff295
Kevin <knguyen3415@gmail.com>
2020-02-17 15:01:39 -0800

    Work on watchlist function

---
+++ frontend/reducers/entities/watchlist_reducer.js
@@ -0,0 +1,27 @@
+import { RECEIVE_WATCHLIST, 
+    RECEIVE_ITEM, 
+    DELETE_ITEM, } from '../../actions/watchlist_actions';
+
+export default (oldState = {}, action) => {
+    Object.freeze(state);
+    
+    switch (action.type) {
+        case RECEIVE_WATCHLIST:
+            return Object.assign({}, action.watchlist);
+
+        case RECEIVE_ITEM:
+            if (action.item) {
+                return Object.assign({}, oldState, { [action.item.id]: action.item });
+            } else {
+                return oldState;
+            };
+
+        case DELETE_ITEM:
+            let newState = Object.assign({}, state);
+            newState[action.itemId] = undefined;
+            return newState;
+    
+        default:
+            return oldState;
+    }
+};
--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -2,10 +2,12 @@ import { combineReducers } from "redux";
 
 import usersReducer from "./entities/users_reducer";
 import shoesReducer from "./entities/shoes_reducer";
+import watchlistsReducer from './entities/watchlist_reducer';
 
 const entitiesReducer = combineReducers({
     users: usersReducer,
-    shoes: shoesReducer
+    shoes: shoesReducer,
+    watchlists: watchlistsReducer
 });
 
 export default entitiesReducer;

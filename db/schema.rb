a1a2c568797a336318bb2adf15d98c388305a284
Kevin <knguyen3415@gmail.com>
2020-03-30 13:03:40 -0700

    Start on follow component

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_26_182448) do
+ActiveRecord::Schema.define(version: 2020_03_30_175740) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,16 @@ ActiveRecord::Schema.define(version: 2020_03_26_182448) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "follows", force: :cascade do |t|
+    t.integer "shoe_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["shoe_id"], name: "index_follows_on_shoe_id"
+    t.index ["user_id", "shoe_id"], name: "index_follows_on_user_id_and_shoe_id", unique: true
+    t.index ["user_id"], name: "index_follows_on_user_id"
   end
 
   create_table "shoes", force: :cascade do |t|
---
+++ frontend/components/follows/follows.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import isEmpty from '../../utils/obj-util';
+import FollowItemContainer from './follow_item_container';
+import { Link } from 'react-router-dom';
+import { withRouter } from 'react-router';
+
+class Follows extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            follows: []
+        }
+    }
+
+    componentDidMount() {
+        let follows = [];
+        this.props.fetchShoes()
+            .then(() => this.props.fetchFollows(this.props.currentUser.id)
+                .then((data) => {
+                    // console.dir(data);
+                    for (let follow of Object.values(data.follows)) {
+                        let shoe = Object.assign({}, this.props.shoes[follow.shoe_id]);
+                        shoe.follow_id = follow.id;
+                        follows.push(shoe);
+                    }
+                    this.setState({ follows: follows });
+                    // console.log(this.state);
+                    // console.log(data.follows);
+                })
+            );
+    }
+
+    render() {
+        let shoes = this.props.shoes;
+        return (
+            <div id="follows-div">
+                <span id="follows-header">Following</span>
+                <table id="follows-table">
+                    <thead id="follows-table-heading">
+                        <tr className="fl-row">
+                            <th className="fl-col0"></th>
+                            <th className="fl-col1">Item</th>
+                            <th className="fl-col2">Highest Bid</th>
+                            <th className="fl-col3">Lowest Ask</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            this.state.follows.map(follow => isEmpty(follow) ? null : <FollowItemContainer follow={follow} key={`follow${follow.f_id}`} />)
+                        }
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+};
+
+export default Follows;

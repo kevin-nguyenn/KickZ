3de649c6e587807a41cb15a293827ca18df6f75c
Kevin <knguyen3415@gmail.com>
2020-03-31 12:12:03 -0700

    Setup orders frontend

--- frontend/util/sale_api_util.js
+++ frontend/util/sale_api_util.js
@@ -1,36 +1,36 @@
-export const fetchSales = (shoe_id) => (
-    $.ajax({
+export const fetchSales = (shoe_id) => {
+    return $.ajax({
         method: 'GET',
         url: `/api/shoes/${shoe_id}/sales`
     })
-);
+};
 
-export const fetchLastSale = (shoe_id) => (
-    $.ajax({
+export const fetchLastSale = (shoe_id) => {
+    return $.ajax({
         method: 'GET',
         url: `/api/shoes/${shoe_id}/sales/last`
     })
-);
+};
 
-export const fetchSalesBySize = (shoe_id, size) => (
-    $.ajax({
+export const fetchSalesBySize = (shoe_id, size) => {
+    return $.ajax({
         method: 'GET',
         url: `/api/shoes/${shoe_id}/sales/${size}`
     })
-);
+};
 
-export const createSale = (sale) => (
-    $.ajax({
+export const createSale = (sale) => {
+    return $.ajax({
         method: 'POST',
         url: '/api/sales',
         data: { sale }
     })
-);
+};
 
-export const updateSale = (sale) => (
-    $.ajax({
+export const updateSale = (sale) => {
+    return $.ajax({
         method: 'PATCH',
         url: `/api/sales/${sale.id}`,
         data: { sale }
     })
-);
+};

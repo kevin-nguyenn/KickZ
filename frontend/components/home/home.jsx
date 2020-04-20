fca21f8fc82bc0e3f5583f76e6072fe2b9ec0a9b
Kevin <knguyen3415@gmail.com>
2020-04-20 12:02:27 -0700

    Work on search function

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -3,6 +3,12 @@ import React from 'react';
 import SlideShow from '../slideshow/slideshow';
 
 class Home extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.handleSearch.bind(this);
+    }
+
     handleSearch(e) {
         e.preventDefault();
         let searchTerm = document.getElementById("navbar-search").value;
@@ -19,16 +25,20 @@ class Home extends React.Component {
             <div>
             <div className="search-container">
                 <div className="search-background">
-                <span className="background">
-                    <div className="search-bar">
-                    <div className="word-set1">Buy and Sell</div>
-                    <div className="word-set2">Authentic Sneakers</div>
-                    <form className="search-input" onChange={this.handleSearch}>
-                        <input type="text" placeholder="🔍 Search for brand..." />
-                        {/* <input type="submit" style={{display: "none"}}/> */}
-                    </form>
-                    </div>
-                </span>
+                    <span className="background">
+                        <div className="search-bar">
+                            <div className="word-set1">Buy and Sell</div>
+                            <div className="word-set2">Authentic Sneakers</div>
+                            <form className="search-input">
+                                <input 
+                                    type="text" 
+                                    placeholder="🔍 Search for brand..."
+                                    defaultValue = ""
+                                    onChange={this.handleSearch}
+                                />
+                            </form>
+                        </div>
+                    </span>
                 </div>
             </div>
 

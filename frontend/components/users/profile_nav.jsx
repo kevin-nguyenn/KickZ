2a57e136201d8f70a814e059c6fb436c4bd2d21f
Kevin <knguyen3415@gmail.com>
2020-04-22 11:40:49 -0700

    Fix bold on profile

--- frontend/components/users/profile_nav.jsx
+++ frontend/components/users/profile_nav.jsx
@@ -4,13 +4,13 @@ import { Link } from 'react-router-dom';
 class ProfileNav extends React.Component {
     constructor(props) {
         super(props);
-        console.log(props)
+        // console.log(props)
 
         this.changeSelected = this.changeSelected.bind(this);
     }
 
     componentDidMount() {
-        $(document.getElementById(`profile-nav-${this.props.selected}`)).addClass('profile-selected');
+        $(document.getElementById(`prof-nav-${this.props.selected}`)).addClass('profile-selected');
     }
 
     changeSelected(e) {
@@ -25,12 +25,12 @@ class ProfileNav extends React.Component {
                 <span className="profile-nav-name">
                     {this.props.currentUser ? this.props.currentUser[1].username : ""}
                 </span>
-                <Link to="/profile/listings">
+                {/* <Link to="/profile/listings">
                     <li id="prof-nav-listings" className="profile-nav-item" onClick={this.changeSelected}>
                         <span className="nav-icon-img">💵</span>
                         <span className="nav-title">Listings</span>
                     </li>
-                </Link>
+                </Link> */}
                 <Link to="/profile">
                     <li id="prof-nav-profile" className="profile-nav-item" onClick={this.changeSelected}>
                         <span className="nav-icon-img">😎</span>

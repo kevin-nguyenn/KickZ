c60d50b458f7483f2edebe8d21d72b1474e408ac
Kevin <knguyen3415@gmail.com>
2020-04-28 13:11:29 -0700

    Fix shoes search import

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -6,9 +6,10 @@ import NavBarContainer from "./nav_bar/nav_bar_container";
 // import SignUpFormContainer from './session_form/signup_form_container';
 // import LogInFormContainer from './session_form/login_form_container';
 import ShoeIndexContainer from './shoes/shoe_index_container'
+import ShoeDetailContainer from './shoes/shoe_detail_container';
+import ShoeSearchContainer from './shoes/shoe_search_container';
 import FormContainer from './session_form/signup_form_container';
 import HomeContainer from './home/home_container';
-import ShoeDetailContainer from './shoes/shoe_detail_container';
 import ProfileContainer from './users/profile_container';
 import Footer from './footer/footer';
 
@@ -26,6 +27,7 @@ const App = () => {
                     <AuthRoute exact path="/signup" component={FormContainer}/>
                     <Route exact path="/shoes" component={ShoeIndexContainer}/>
                     <Route path="/shoes/:shoeId" component={ShoeDetailContainer}/>
+                    <Route path="/shoes/search" component={ShoeSearchContainer}/>
                     <ProtectedRoute path="/profile" component={ProfileContainer}/>
                     <Route exact path="/" component={HomeContainer}/>
                 </Switch>

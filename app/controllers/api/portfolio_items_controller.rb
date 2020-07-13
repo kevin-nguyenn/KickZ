6bf8325a680022a1eec9809249a806d39d0054a9
Kevin <knguyen3415@gmail.com>
2020-07-13 12:21:01 -0700

    Setup portfolio controller

---
+++ app/controllers/api/portfolio_items_controller.rb
@@ -0,0 +1,36 @@
+class Api::PortfolioItemsController < ApplicationController
+    def index
+        @items = current_user.portfolio_items
+    end
+
+    def create
+        @item = PortfolioItem.new(item_params)
+        if @item.save
+            render :show
+        else
+            render json: @item.errors.full_messages, status: 401
+        end
+    end
+
+    def update
+        @item = PortfolioItem.find(params[:id])
+        if @item.update(item_params)
+            render :show
+        else
+            render json: @item.errors.full_messages, status: 401
+        end
+    end
+
+    def show
+        @item = PortfolioItem.find(params[:id])
+    end
+
+    def destroy
+        @item = PortfolioItem.find(params[:id])
+        @item.destroy
+    end
+
+    def item_params
+        params.require(:item).permit(:id, :shoe_id, :user_id, :size, :purchase_price)
+    end
+end
--- app/controllers/portfolio_items_controller.rb
+++
@@ -1,2 +0,0 @@
-class PortfolioItemsController < ApplicationController
-end

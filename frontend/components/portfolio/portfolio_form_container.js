248655e9be5dca4aae300c4c4cdb1ca5bed2a258
Kevin <knguyen3415@gmail.com>
2020-08-18 16:32:56 -0700

    Work on portfolio form

---
+++ frontend/components/portfolio/portfolio_form_container.js
@@ -0,0 +1,25 @@
+import { connect } from "react-redux";
+import { addItem } from "../../actions/portfolio";
+import { fetchShoes } from "../../actions/shoe_actions";
+import { resetErrors } from "../../actions/errors";
+// import { fetchPortfolio } from "../../utils/api-portfolio-util";
+
+import PortfolioForm from "./portfolio_form";
+
+const mapStateToProps = (state) => {
+  let isLoggedIn = state.session.id ? true : false;
+  return ({
+    currentUser: (isLoggedIn ? state.entities.users : null),
+    shoes: state.entities.shoes,
+    portfolio: state.entities.portfolio,
+    errors: state.errors.portfolio,
+  });
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  addItem: (id, item) => dispatch(addItem(id, item)),
+  fetchShoes: () => dispatch(fetchShoes()),
+  resetErrors: () => dispatch(resetErrors()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(PortfolioForm);
